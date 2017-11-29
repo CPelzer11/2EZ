d866e4ac6b7b1aeec1f5605b322e03f85a33c89a
benjamis <33106390+benjamis@users.noreply.github.com>
2017-11-29 08:43:34 -0600

    confirmation

---
+++ confirmation.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+	
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="login.js"></script>
+        <title>Confirmation</title>
+    </head>
+    <body>
+		
+        <div id="main">
+            <img class="img-rounded img-responsive"src="images/banner2.png" id="banner">
+            <h1>Successful Submission!</h1>
+            <h4 id="submission">Your evaluation has been successfully submitted, would you like to fill out another?</h4>
+            <table>
+                <tr>
+                    <td>
+                        <a href="login.php" class="btn btn-secondary">Logout</a>
+                    </td>
+                    <td>
+                        <a href="index.php" class="btn btn-secondary">New Evaluation</a>
+                    </td>
+                </tr>
+            </table>
+            <img src="images/WSU_GreenFee.png" id="greenFee" class="img-responsive">
+    
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -60,8 +60,23 @@
                     </thead>
 
                     <tbody id="myTable">
+                        <tr>
+                            <td>
+                                2EZ
+                            </td>
+                            <td>
+                                12.09.16
+                            </td>
+                            <td>
+                                Gary Garyson
+                            </td>
+                            <td>
+                                <a href="forms.php" class="btn btn-secondary">Review
+                                </a>
+                            </td>
+                        </tr>
                       <?php
-                        $dbconnect = mysqli_connect('localhost', 'root', 'Tjriah_1204','cs344proj');
+                        $dbconnect = mysqli_connect('localhost', 'root', '','WEBDB');
                         $find = "SELECT title, contact_name FROM project;";
                         $result= mysqli_query($dbconnect, $find);
                         if(mysqli_num_rows($result)>0){
@@ -75,7 +90,7 @@
                               <td>
                             <?php print($show["contact_name"]);?>
                               </td>
-                              <td><a href="forms.php" class="btn btn-secondary">Review</td>
+                                <td><a href="forms.php" class="btn btn-secondary">Review</a></td>
                           <?php
                             }//end while
                             }//end if
@@ -87,7 +102,7 @@
 
               <div class="pagination">
                     <ul class="pagination">
-                      <?php
+                      <?/*php
                         $x = $count/2;
                         $y = ceil($x);
                         for($i = 1; $i < $y; $i++) {
@@ -95,7 +110,7 @@
                           <li class="page-item"><a class="page-link" href = ""><?= $i?></a></li>
                       <?php
                         }
-                      ?>
+                      */?>
                     </ul>
                   </div>
               </div>
